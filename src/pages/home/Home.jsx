5d4ded38efc83c523e209620d2461453745475a2
lsy8658 <socszz@gmail.com>
2023-11-14 18:04:11 +0900

    home 디자인 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import WeatherBox from "../../components/weatherBox/WeatherBox";
 import TextAnimation from "../../components/textAnimation/TextAnimation.jsx";
+import { Link } from "react-router-dom";
 import "./home.scss";
 export default function Home() {
   return (
     <div className="home_container">
       <TextAnimation text={"음악전공에서 개발자로"} />
       <p className="sub_txt">프론트앤드 이성윤입니다.</p>
+
       <WeatherBox />
     </div>
   );
